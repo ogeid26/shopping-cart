3f98d0e108a830db6a34d7af5b3ed6824241fc83
ogeid26 <dmalvaradog26@gmail.com>
2023-06-04 20:52:03 -0300

    filter state for all app

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,22 +1,24 @@
 import { useId, useState } from 'react'
 import './Filters.css'
+import { useFilters } from '../hooks/useFilters'
 
-export function Filters ( {onChange} ) {
-    const [minPrice, setMinPrice] = useState(0)
+export function Filters () {
+    const {filters, setFilters} =  useFilters()
+
     const minPriceFilterId = useId()
     const categoryFilterId = useId()
 
 
     const handleChangeMinPrice = (event) => {
-    setMinPrice(event.target.value)
-    onChange(prevState => ({
+
+    setFilters(prevState => ({
         ...prevState, 
         minPrice: event.target.value
     }))
     }
 
     const handleChangeCategory = (event) => {
-        onChange(prevState => ({
+        setFilters(prevState => ({
         ...prevState, 
         category: event.target.value
     }))
@@ -34,8 +36,9 @@ export function Filters ( {onChange} ) {
             min='0'
             max='1000'
             onChange={handleChangeMinPrice}
+            value = {filters.minPrice}
             />  
-            <span>${minPrice}</span>      
+            <span>${filters.minPrice}</span>      
         </div>
 
         <div>
