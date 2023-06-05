2ee28b24edfbde67bd383ca2fd29292cf5d6ae5f
ogeid26 <dmalvaradog26@gmail.com>
2023-06-04 21:09:49 -0300

    added cart

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
 import { IS_DEVELOPMENT } from "./config"
 import { useFilters } from "./hooks/useFilters"
+import { Cart } from "./components/Cart"
 
 
 
@@ -18,6 +19,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Cart />
      <Products products={filteredProducts} />
      {IS_DEVELOPMENT && <Footer />}
     </>
